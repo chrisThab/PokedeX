a347508cf903f2c6ca63f732138785b66cc8c119
chrisThab <christianthabar@gmail.com>
2025-08-16 17:33:52 +0200

    done some design and added api with js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+const FORMS_URL = "https://pokeapi.co/api/v2/pokemon-form/";
+
+async function loadData() {
+  loadPokemonData("/1/");
+}
+
+async function loadPokemonData(path='') {
+  let response = await fetch(BASE_URL + path);
+  let responseJson = await response.json();
+
+  document.getElementById('numberOfPokemon').innerHTML = responseJson.id;
+  document.getElementById('nameOfPokemon').innerHTML = responseJson.name;
+  document.getElementById('pic').src = responseJson.sprites.other.dream_world.front_default;
+}
