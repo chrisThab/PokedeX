33ace00f4eea1a74efe969137c6057e34347b3e5
chrisThab <christianthabar@gmail.com>
2025-08-18 20:01:18 +0200

    for loop implemented 1st 20 pokemon

--- scripts/dialogSearch.js
+++ scripts/dialogSearch.js
@@ -3,8 +3,14 @@ function toggleDialog(){
   document.getElementById('detailView').classList.toggle('dialogVisible');
 }
 
-async function search(searchString) {
-  console.log(`Searchstring: ${searchString}`);
-  let found = await fetch(BASE_URL + path);
-  let foundjso
-}
+const searchInput = document.getElementById('searchFor');
+searchInput.addEventListener('input', e =>{
+  const value = e.target.value;
+  console.log(value);
+
+})
+// async function search(searchString) {
+//   console.log(`Searchstring: ${searchString}`);
+//   let found = await fetch(BASE_URL + path);
+//   let foundJson = await found.text();
+// }
--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,83 @@
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 let container = document.getElementById('main');
 
 async function loadData() {
-  loadPokemonData("/1/");
+  await twentyOfEm();
 }
 
 async function loadPokemonData(path='') {
   let response = await fetch(BASE_URL + path);
   let responseJson = await response.json();
   
-  document.getElementById('numberOfPokemon').innerHTML = '#' + responseJson.id;
-  document.getElementById('nameOfPokemon').innerHTML = responseJson.name.toUpperCase();
-  document.getElementById('pic').src = responseJson.sprites.other.dream_world.front_default;
+  buildCard(responseJson);
 }
 
 async function twentyOfEm(){
-  for (let i = 0; i <= 20; i++) {
-    const response = await fetch(BASE_URL + i);
-    const data = await response.json();
-
-    
+  for (let i = 1; i <= 20; i++) {
+    await loadPokemonData(i);
+    };
   }
+
+function buildCard(pokemon){
+  const card = document.createElement('div');
+  card.className= 'card borderStandard';
+
+  card.innerHTML = `
+    <div class="nameNumber borderStandard">
+      <p>#${pokemon.id}</p>
+      <p>${pokemon.name.toUpperCase()}</p>
+    </div>
+    <div class="pokemonPicture" id="picturePokemon">
+      <img onclick="toggleDialog()" src="${pokemon.sprites.other.dream_world.front_default}" alt="picture of ${pokemon.name}">
+    </div>
+    <div class="pokemonDetails">
+      <div onclick="" class="borderStandard">abilities</div>
+      <div onclick="" class="borderStandard">stats</div>
+      <div onclick="" class="borderStandard">evolution</div>
+    </div>
+  `;
+  
+  container.appendChild(card);
 }
+
+// const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+// let container = document.getElementById('main');
+
+// async function loadData() {
+//   await loadPokemonData();
+//   await loadTwentyPokemon();
+// }
+
+// async function loadPokemonData(id) {
+//   const response = await fetch(BASE_URL + id);
+//   const data = await response.json();
+//   buildCard(data);
+// }
+
+// async function loadTwentyPokemon() {
+//   for (let i = 1; i <= 20; i++) {
+//     await loadPokemonData(i);
+//   }
+// }
+
+// function buildCard(pokemon) {
+//   const card = document.createElement('div');
+//   card.className = 'card borderStandard';
+
+//   card.innerHTML = `
+//     <div class="nameNumber borderStandard">
+//       <p>#${pokemon.id}</p>
+//       <p>${pokemon.name.toUpperCase()}</p>
+//     </div>
+//     <div class="pokemonPicture" id="picturePokemon">
+//       <img onclick="toggleDialog()" src="${pokemon.sprites.other.dream_world.front_default}" alt="picture of ${pokemon.name}">
+//     </div>
+//     <div class="pokemonDetails">
+//       <div onclick="" class="borderStandard">abilities</div>
+//       <div onclick="" class="borderStandard">stats</div>
+//       <div onclick="" class="borderStandard">evolution</div>
+//     </div>
+//   `;
+
+//   container.appendChild(card);
+// }
